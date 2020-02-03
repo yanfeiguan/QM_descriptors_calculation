4cfde4551c43f229b1e585c66c49776a72b2c9b8
Yanfei Guan <yanfeig@mit.edu>
2020-02-02 19:45:22 -0500

    starting the project

---
+++ utils.py
@@ -0,0 +1,18 @@
+import logging
+
+
+def create_logger(name: str) -> logging.Logger:
+    """
+    Creates a logger with a stream handler and two file handlers.
+
+    The stream handler prints to the screen depending on the value of `quiet`.
+    One file handler (verbose.log) saves all logs, the other (quiet.log) only saves important info.
+
+    :param save_dir: The directory in which to save the logs.
+    :return: The logger.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    return logger
